d75fe77972c66251ee6eccf3946d93d4eccab113
Arham Hussain 🥈 <153752282+ARHAM5432@users.noreply.github.com>
2024-10-10 17:45:06 +0500

    Updated 🧠

---
+++ app.js
@@ -0,0 +1,59 @@
+// /Q1💫
+// var number = prompt("Enter a positive number");
+// var num = parseFloat(number);
+// alert("Number: " + number);
+// alert("Round off value: " + Math.round(number));
+// alert("Floor value: " + Math.floor(number));
+// alert("Ceil value: " + Math.ceil(number));
+
+
+// Q2💫
+// var number = prompt("Enter a negative number");
+// var num = parseFloat(number);
+// alert("Number: " + number);
+// alert("Round off value: " + Math.round(number));
+// alert("Floor value: " + Math.floor(number));
+// alert("Ceil value: " + Math.ceil(number));
+
+
+
+// Q3💫
+// var a = +prompt("Enter a  number")
+// console.log("absolute value: " + Math.abs(a));
+
+
+// Q4💫
+// var ran=Math.floor(Math.random()*6)+1;
+// console.log("Value of dice is: "+ran);
+
+
+// Q5💫
+// var ran=Math.floor(Math.random()*2) + 1;
+// if(ran===1){
+//     console.log("Random coin value: Heads");
+// }
+// else{
+//     console.log("Random coin value: Tail");
+// }
+
+
+// Q6💫
+// var ran=Math.round(Math.random()*100 )+1;
+// console.log("random Number between 1 and 100 is: "+ran);
+
+
+// Q7💫
+// var userInput = prompt("Enter your weight (e.g., 50, 50kgs, 50.2kgs, 50.2kilograms):");
+// var weight = parseFloat(userInput);
+// console.log("The weight of user is "+ weight +" kilograms");
+
+
+// Q8💫
+// var n = Math.random() * 10 + 1;
+// var promp = prompt("Enter a number between 1 and 10")
+// if (n == promp) {
+//     console.log("Congrats! 🤍");
+// }
+// else {
+//     console.log("Numbers are not same , sorry!");
+// }
